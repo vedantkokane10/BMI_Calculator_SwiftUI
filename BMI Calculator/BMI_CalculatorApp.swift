bd6a9cdd076c119ecc2a8e8e126eec692de62730
Vedant Kokane <kokanevedant@gmail.com>
2023-07-01 22:39:14 +0530

    Initial Commit

---
+++ BMI Calculator/BMI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorApp.swift
+//  BMI Calculator
+//
+//  Created by Vedant Kokane on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
